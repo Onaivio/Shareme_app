275962d21265aba6c5631e8ed8511ae082724ea9
Onaivio <107544743+Onaivio@users.noreply.github.com>
2022-12-29 10:23:59 +0100

    ..

--- frontend/src/container/Pins.jsx
+++ frontend/src/container/Pins.jsx
@@ -9,7 +9,8 @@ const Pins = ({ user }) => {
   return (
     <div className="px-2 md:px-5">
       <div className="bg-gray-50">
-        <Navbar searchTerm={searchTerm} setSearchTerm={setSearchTerm} user={user && user} />
+        <Navbar />
+        {/* <Navbar searchTerm={searchTerm} setSearchTerm={setSearchTerm} user={user && user} /> */}
       </div>
       <div className="h-full">
         <Routes>
